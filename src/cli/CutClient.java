5ca726d1da3dc7ee69945e03de486d1a1e9ae8d9
prajakta <prajaktv@thoghtworks.com>
2014-01-11 11:41:25 +0530

    Cut basic functionality completed.

---
+++ src/cli/CutClient.java
@@ -0,0 +1,31 @@
+package cli;
+
+import prajaktv.fileReader.FileReader;
+import prajaktv.unixtools.Cut;
+
+public class CutClient {
+    public static void main(String[] args) {
+        Cut lib = new Cut();
+        String delimeter = " ";
+        int feild = 0;
+        String filename = "";
+        if (args.length == 3) {
+            filename = args[0];
+            delimeter = args[2];
+            if (args[1].startsWith("-")) {
+                feild = Integer.parseInt(args[1].substring(1));
+            }
+        }
+        if (args.length == 2) {
+            filename = args[0];
+            if (args[1].startsWith("-")) {
+                feild = Integer.parseInt(args[1].substring(1));
+                delimeter = " ";
+            }
+        }
+        FileReader fr = new FileReader();
+        String data = fr.readFile(filename);
+
+        lib.cutCount(data, feild, delimeter);
+    }
+}
---
+++ src/prajaktv/unixtools/Cut.java
@@ -0,0 +1,19 @@
+package prajaktv.unixtools;
+
+
+public class Cut {
+    public void cutCount(String filedata, int field_no, String delimeter) {
+        String words[];
+        String result = "";
+        int i;
+        String data[] = filedata.split("\n");
+        for (i = 0; i < data.length; i++) {
+            words = data[i].split(delimeter);
+            if (words.length <= field_no)
+                result = result + "\r\n";
+            else
+                result = result + words[field_no - 1] + "\r\n";
+        }
+        System.out.println(result);
+    }
+}
