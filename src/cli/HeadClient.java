a40fd5ffb9a52900d33f8d8b3293f37634956faf
prajakta <prajaktv@thoghtworks.com>
2014-01-11 09:50:08 +0530

    some changes in head and tail for args options

--- src/cli/HeadClient.java
+++ src/cli/HeadClient.java
@@ -6,7 +6,12 @@ import prajaktv.unixtools.Head;
 public class HeadClient {
     public static void main(String[] args) {
         Head lib = new Head();
-        String filename = args[1];
+        String filename;
+        if(args.length == 2){
+        filename = args[1];
+        }
+        else
+        filename = args[0];
         FileReader fr = new FileReader();
         String data = fr.readFile(filename);
         lib.head(data, args);
--- src/cli/TailClient.java
+++ src/cli/TailClient.java
@@ -6,9 +6,13 @@ import prajaktv.unixtools.Tail;
 public class TailClient {
     public static void main(String[] args) {
         Tail lib = new Tail();
-        String filename = args[1];
+        String filename;
+        if (args.length == 2) {
+            filename = args[1];
+        } else
+            filename = args[0];
         FileReader fr = new FileReader();
         String data = fr.readFile(filename);
-        lib.tail(data,args);
+        lib.tail(data, args);
     }
 }
